3f784ac237b33cdb20069b3942d93730ab9a3bfa
Kupcha <boris.kupcha@intel.com>
2020-10-25 09:54:59 +0200

    created OFDM.py with a simple model of a complex exp in frequency domain

---
+++ OFDM.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from commpy.filters import rrcosfilter
+from scipy import signal
+import math
+
+# Tsym = (3.2 + 0.8) * 1e-6  # 3.2 [uS] symbol time + 0.8[uS] Long GI
+Tsym = 1
+Delta_F = 1 / Tsym
+# F = Delta_F
+F = 10   # analog Frequency
+# F_samp = 4 * Delta_F  # sample by nyquist
+F_samp = 20*F  # sample frquency
+# F_axis = np.arange(-2 * Delta_F, 2 * Delta_F, Delta_F)
+
+t = np.arange(0, Tsym, 1 / F_samp)
+F_axis = np.arange(-F_samp/2, F_samp/2, F_samp/(len(t)-1))
+# k = 1
+S_t = np.exp(-1j * 2 * np.pi * F * t)
+# S_t = np.cos(2*np.pi*F*t)
+S_f = np.fft.fft(S_t)
+# plt.plot(t, S_t)
+# plt.xlabel('Time')
+# plt.ylabel('S(t)')
+plt.plot(F_axis, S_f)
+plt.xlabel('Frequency')
+plt.ylabel('S(f)')
+plt.show()
+
+print(S_t)
