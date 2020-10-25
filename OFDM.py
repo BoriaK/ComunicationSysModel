c9c0cdfd66aa42ca5bdd6b7c78bb0f416924a584
Boris <boria21@gmail.com>
2020-10-25 16:56:42 +0200

    place all 64 carriers on the frequency domain

--- OFDM.py
+++ OFDM.py
@@ -4,30 +4,34 @@ from commpy.filters import rrcosfilter
 from scipy import signal
 import math
 
-# Tsym = (3.2 + 0.8) * 1e-6  # 3.2 [uS] symbol time + 0.8[uS] Long GI
-Tsym = 1
+GI = 0.8 * 1e-6  # 0.8[uS] Long GI
+Tsym = 3.2 * 1e-6  # 3.2 [uS] symbol time
 Delta_F = 1 / Tsym
 # F = Delta_F
-F = 10   # analog Frequency
+F = 1 / Tsym  # analog Frequency
 # F_samp = 4 * Delta_F  # sample by nyquist
-F_samp = 20*F  # sample frquency
+F_samp = 20*1e6  # sample frquency 20MHz
 # F_axis = np.arange(-2 * Delta_F, 2 * Delta_F, Delta_F)
 
 t = np.arange(0, Tsym, 1 / F_samp)
-F_axis = np.arange(-F_samp/2, F_samp/2, F_samp/(len(t)-1))
+F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
 # k = 1
-S_t1 = np.exp(-1j * 2 * np.pi * F * t)
-S_t2 = np.exp(-1j * 2 * np.pi * (-F) * t)
-# S_t = np.cos(2*np.pi*F*t)
-S_f1 = (1/len(S_t1))*np.fft.fft(S_t1)
-S_f2 = (1/len(S_t2))*np.fft.fft(S_t2)
+CW_t = {}
+CW_f = {}
+for k in range(len(F_axis)):
+    CW_t[k] = np.exp(1j * 2 * np.pi * (k-len(F_axis)/2) * F * t)
+    CW_f[k] = (1 / len(CW_t[k])) * np.fft.fft(CW_t[k])
+
 # plt.plot(t, S_t)
 # plt.xlabel('Time')
 # plt.ylabel('S(t)')
-plt.plot(F_axis, np.fft.fftshift(S_f1), F_axis, np.fft.fftshift(S_f2))
+# plt.plot(F_axis, np.fft.fftshift(S_f11))
+plt.figure()
+for k in range(len(CW_t)):
+    plt.plot(F_axis, np.fft.fftshift(CW_f[k]))
 plt.grid()
 plt.xlabel('Frequency')
 plt.ylabel('S(f)')
 plt.show()
 
-print(S_t1)
+print(S_t11)
