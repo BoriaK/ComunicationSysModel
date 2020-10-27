0afcb2596d0a354379772600b23c6c6db58282af
Kupcha <boris.kupcha@intel.com>
2020-10-27 13:22:32 +0200

    added summation to the siganl in frequency domain, along frequency axes

--- OFDM.py
+++ OFDM.py
@@ -19,10 +19,10 @@ F_samp = 20 * 1e6  # sample frquency 20MHz
 t = np.arange(0, Tsym, 1 / F_samp)
 F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
 # k = 1
-CW_t = {}
-CW_f = {}
-S_t = {}
-S_f = {}
+CW_tk = []
+CW_fk = []
+S_tk = []
+S_fk = []
 
 # Sinc Pulse:
 X_t = np.sinc(2 * np.pi * F * t)
@@ -41,40 +41,56 @@ g_SRRC = np.sqrt(2) * rrcosfilter(N_samp, alpha, Tsym, F_samp)[1]
 # populating the negative tones:
 for k in range(int(len(F_axis)/2)):
     if (k - len(F_axis) / 2) < -28:
-        CW_t[k] = 0  # maybe will need an array of zeros?
-        CW_f[k] = 0
+        # CW_tk[k] = [0]  # maybe will need an array of zeros?
+        # CW_fk[k] = [0]
+        CW_tk.append(0)
+        CW_fk.append(0)
     else:
-        CW_t[k] = np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
-        CW_f[k] = (1 / len(CW_t[k])) * np.fft.fft(CW_t[k])
+        # CW_tk[k] = np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
+        # CW_fk[k] = (1 / len(CW_tk[k])) * np.fft.fft(CW_tk[k])
+        CW_tk.append(np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t))
+        CW_fk.append((1 / len(CW_tk[k])) * np.fft.fft(CW_tk[k]))
 
-    S_t[k] = Dta[k]*X_t * CW_t[k]
+    # S_tk[k] = Dta[k]*X_t * CW_tk[k]
+    # S_tk.append(Dta[k]*X_t * CW_tk[k])
     # S_t[k] = g_SRRC * CW_t[k]
-    S_f[k] = (1 / len(S_t[k])) * np.fft.fft(S_t[k])
+    S_tk.append(Dta[k] * g_SRRC * CW_tk[k])
+    # S_fk[k] = (1 / len(S_tk[k])) * np.fft.fft(S_tk[k])
+    S_fk.append((1 / len(S_tk[k])) * np.fft.fft(S_tk[k]))
 
 # populating the positive tones:
 for k in range(int(len(F_axis)/2), len(F_axis)):
     if (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-        CW_t[k] = 0
-        CW_f[k] = 0
-    else:
-        CW_t[k] = np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
-        CW_f[k] = (1 / len(CW_t[k])) * np.fft.fft(CW_t[k])
+        # CW_tk[k] = 0
+        # CW_fk[k] = 0
+        CW_tk.append(0)
+        CW_fk.append(0)
 
-    S_t[k] = Dta[k]*X_t * CW_t[k]
+    else:
+        # CW_tk[k] = np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
+        # CW_fk[k] = (1 / len(CW_tk[k])) * np.fft.fft(CW_tk[k])
+        CW_tk.append(np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t))
+        CW_fk.append((1 / len(CW_tk[k])) * np.fft.fft(CW_tk[k]))
+
+    # S_tk[k] = Dta[k]*X_t * CW_tk[k]
+    S_tk.append(Dta[k] * X_t * CW_tk[k])
     # S_t[k] = g_SRRC * CW_t[k]
-    S_f[k] = (1 / len(S_t[k])) * np.fft.fft(S_t[k])
+    S_tk.append(Dta[k] * g_SRRC * CW_tk[k])
+    # S_fk[k] = (1 / len(S_tk[k])) * np.fft.fft(S_tk[k])
+    S_fk.append((1 / len(S_tk[k])) * np.fft.fft(S_tk[k]))
 
 # add summation along the frequency axis:
+S_f = np.sum(S_fk, axis=0)
 
-
-# plt.plot(F_axis, np.fft.fftshift(S_f[int(len(F_axis)/2)]))   # plot the middle tone only
-# plt.plot(F_axis, np.fft.fftshift(S_f[int(len(F_axis)/2)-1]))
-# plt.show()
+plt.plot(F_axis, np.fft.fftshift(S_f))
+# plt.plot(F_axis, np.fft.fftshift(S_f[int(len(F_axis)/2)-1])) # plot the middle tone only
+plt.grid()
+plt.show()
 plt.figure()
-# for k in range(len(CW_f)):
-#     plt.plot(F_axis, np.fft.fftshift(CW_f[k]))
-for k in range(len(S_f)):
-    plt.plot(F_axis, np.fft.fftshift(S_f[k]))
+# for k in range(len(CW_fk)):
+#     plt.plot(F_axis, np.fft.fftshift(CW_fk[k]))
+for k in range(len(S_fk)):
+    plt.plot(F_axis, np.fft.fftshift(S_fk[k]))
 plt.grid()
 plt.xlabel('Frequency')
 plt.ylabel('S(f)')
