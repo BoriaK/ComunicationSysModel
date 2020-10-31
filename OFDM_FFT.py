60dede1d68de3e4b79e3aa2249d7c4baf51bf806
Boris <boria21@gmail.com>
2020-10-31 11:47:49 +0200

    fixed OFDM_Oscilator and OFDM_FFT up to D/A converter block

--- OFDM_FFT.py
+++ OFDM_FFT.py
@@ -19,100 +19,59 @@ Dta = m_i + 1j * m_q
 GI = 0.8 * 1e-6  # 0.8[uS] Long GI
 Tsym = 3.2 * 1e-6  # 3.2 [uS] symbol time
 Delta_F = 1 / Tsym
-# F = Delta_F
 F = 1 / (Tsym + GI)  # analog Frequency
 F_samp = 20 * 1e6  # sample frquency 20MHz
 # F_axis = np.arange(-2 * Delta_F, 2 * Delta_F, Delta_F)
 
 t = np.arange(0, Tsym, 1 / F_samp)
 F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
-# k = 1
-CW_tk = []
-CW_fk = []
-S_tk = []
-S_fk = []
+S_t = np.zeros(len(F_axis), dtype=np.complex)
+S_f = np.zeros(len(F_axis), dtype=np.complex)
+S_tk = np.zeros(len(F_axis), dtype=np.complex)
 
 # prepare the data in frequency domain:
 skip = 0
 for k in range(len(F_axis)):
     if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-        CW_tk.append(np.zeros(len(t)))
         skip += 1
+        S_f[k] = 0
     else:
-        # CW_tk.append((1 / np.sqrt(Tsym)) * np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t))
-        CW_tk.append(np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t))
-    CW_fk.append((1 / len(F_axis)) * np.fft.fft(CW_tk[k]))
-    S_fk.append(np.convolve(Dta[k - skip], CW_fk[k]))
+        S_f[k] = Dta[k - skip]
+
+# prepare time domain signal using IFFT:
+S_t = np.fft.ifft(S_f, n=64)
 
 plt.figure()
-plt.plot(F_axis, np.fft.fftshift(CW_fk))
-plt.xlabel('Frequency')
-plt.ylabel('CW_k(f)')
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.plot(F_axis, np.fft.fftshift(S_fk))
-plt.xlabel('Frequency')
-plt.ylabel('S_k(f)')
-plt.grid()
-plt.show()
-
-# not sure weather it's necessary
-S_f = np.sum(S_fk, axis=0)
-CW_f = np.sum(CW_fk, axis=0)
-
-plt.figure()
-plt.plot(F_axis, np.fft.fftshift(CW_f))
-plt.xlabel('Frequency')
-plt.ylabel('CW(f)')
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.plot(F_axis, np.abs(np.fft.fftshift(S_f)))
-# plt.plot(F_axis, np.abs(S_f))
+plt.plot(F_axis, np.abs(S_f))
 plt.xlabel('Frequency')
 plt.ylabel('S(f)')
 plt.grid()
 plt.show()
 
-# generating the time OFDM signal:
-S_tk = np.fft.ifft(S_fk, axis=-1)
-plt.figure()
-plt.plot(t, S_tk)
-plt.xlabel('Time')
-plt.ylabel('S_k(t)')
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.subplot(3, 1, 1)
-plt.plot(t, S_tk[4])
-plt.xlabel('Time')
-plt.ylabel('S_1(t)')
-plt.grid()
-
-plt.subplot(3, 1, 2)
-plt.plot(t, S_tk[15])
-plt.xlabel('Time')
-plt.ylabel('S_5(t)')
-plt.grid()
-
-plt.subplot(3, 1, 3)
-plt.plot(t, S_tk[25])
-plt.xlabel('Time')
-plt.ylabel('S_15(t)')
-plt.grid()
-
-plt.show()
-
-# combined option:
-S_t = np.fft.ifft(S_f)
 plt.figure()
 plt.plot(t, S_t)
 plt.xlabel('Time')
 plt.ylabel('S(t)')
 plt.grid()
 plt.show()
+
+# Parallel to serial: ?
+
+# incerting cyclic prefix:
+S_t_w_CP = np.zeros(int(len(S_t)+16), dtype=np.complex)
+CP = S_t[range(int(len(S_t)-16), len(S_t))]
+S_t_w_CP[range(16)] = CP
+S_t_w_CP[range(16, len(S_t_w_CP))] = S_t
+
+t_w_CP = np.arange(0, Tsym+GI, 1 / F_samp)  # new Symbol time includes cyclic prefix
+
+plt.figure()
+plt.plot(t_w_CP, S_t_w_CP)
+plt.xlabel('Time')
+plt.ylabel('S(t) with GI')
+plt.grid()
+plt.show()
+
+
+
 print('')
--- OFDM_Oscilator.py
+++ OFDM_Oscilator.py
@@ -7,10 +7,7 @@ import math
 # random 56 symbols of data
 rng = np.random.default_rng()
 
-# BPSK
-# Dta = 2 * rng.integers(1, high=2, size=56, dtype=np.int64, endpoint=True) - 3
-
-# QPSK
+# 16QAM
 M = 16
 # infase data
 m_i = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
@@ -19,59 +16,72 @@ m_q = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoin
 
 Dta = m_i + 1j * m_q
 
+# BPSK
+# Dta = 2 * rng.integers(1, high=2, size=56, dtype=np.int64, endpoint=True) - 3
+
 GI = 0.8 * 1e-6  # 0.8[uS] Long GI
 Tsym = 3.2 * 1e-6  # 3.2 [uS] symbol time
 Delta_F = 1 / Tsym
-# F = Delta_F
 F = 1 / (Tsym + GI)  # analog Frequency
 F_samp = 20 * 1e6  # sample frquency 20MHz
-# F_axis = np.arange(-2 * Delta_F, 2 * Delta_F, Delta_F)
 
 t = np.arange(0, Tsym, 1 / F_samp)
 F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
-# k = 1
-CW_tk = []
-CW_fk = []
-S_tk = []
-S_fk = []
 
-# Square Root raised cosine pulse: Not nessesary at this point
-N_samp = len(t)
-alpha = 0.25  # roll off factor
-g_SRRC = rrcosfilter(N_samp, alpha, GI + Tsym, F_samp)[1]
-# plt.plot(t, g_SRRC)
-# plt.show()
-################################################################
-# creating the symbols in frequency domain:
+CW_tk = np.zeros((len(F_axis), len(t)), dtype=np.complex)
+S_tk = np.zeros((len(F_axis), len(t)), dtype=np.complex)
 
+# prepare the data in frequency domain:
 skip = 0
 for k in range(len(F_axis)):
     if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-        CW_tk.append(np.zeros(len(t)))
         skip += 1
     else:
-        CW_tk.append((1 / np.sqrt(Tsym)) * np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t))
-    S_tk.append(Dta[k-skip] * CW_tk[k])
+        # CW_tk[k][:] = (1 / np.sqrt(Tsym)) * np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
+        CW_tk[k][:] = np.exp(1j * 2 * np.pi * (k - len(F_axis) / 2) * Delta_F * t)
+    S_tk[k][:] = Dta[k - skip]*CW_tk[k]
 
-
-# add summation along the frequency axis:
-# this is the signal combined of all the symbols in time:
 S_t = np.sum(S_tk, axis=0)
 
-# The signal with all the Symbols in frequency domain:
-S_f = (1 / len(S_t)) * np.fft.fft(S_t)
+# Plot the OFDM Symbol in time
+plt.figure()
+plt.plot(t, S_t)
+plt.xlabel('Time')
+plt.ylabel('S_(t)')
+plt.grid()
+plt.show()
 
-plt.figure(1)
-plt.plot(F_axis, np.fft.fftshift(S_f))
+# Plot some Carriers of the OFDM symbol in time
+plt.figure()
+plt.subplot(3, 1, 1)
+plt.plot(t, S_tk[4])
+plt.xlabel('Time')
+plt.ylabel('S_4(t)')
+plt.grid()
+
+plt.subplot(3, 1, 2)
+plt.plot(t, S_tk[15])
+plt.xlabel('Time')
+plt.ylabel('S_15(t)')
+plt.grid()
+
+plt.subplot(3, 1, 3)
+plt.plot(t, S_tk[25])
+plt.xlabel('Time')
+plt.ylabel('S_25(t)')
+plt.grid()
+
+plt.show()
+
+# OFDM Symbol in frequency:
+S_f = np.fft.fft(S_t)
+
+plt.figure()
+plt.plot(F_axis, np.abs(np.fft.fftshift(S_f)))
 plt.xlabel('Frequency')
 plt.ylabel('S(f)')
 plt.grid()
 plt.show()
-plt.figure(2)
-plt.plot(t, S_t)
-plt.xlabel('Time')
-plt.ylabel('S(t)')
-plt.grid()
-plt.show()
+
 
 print('')
