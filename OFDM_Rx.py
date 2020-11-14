75aa6aa8b77e750a86de82ae383ecfbcfc0eb4f4
Kupcha <boris.kupcha@intel.com>
2020-11-14 13:36:20 +0200

    finished Rx model for 10 data chunks

--- OFDM_Rx.py
+++ OFDM_Rx.py
@@ -5,15 +5,16 @@ from scipy import signal
 import math
 
 # Tx
-# random 56 symbols of data
+Num_Dta_chnk = 10  # number of data chunks
+# random 56 symbols of data per packet
 rng = np.random.default_rng()
 
 # 16QAM
 M = 16
 # infase data
-m_i = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
+m_i = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56*Num_Dta_chnk, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
 # quadrature data
-m_q = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
+m_q = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56*Num_Dta_chnk, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
 
 Tx_Dta = m_i + 1j * m_q
 
@@ -23,80 +24,91 @@ Delta_F = 1 / Tsym
 F = 1 / (Tsym + GI)  # analog Frequency
 F_samp = 20 * 1e6  # sample frquency 20MHz
 
-t = np.arange(0, Tsym, 1 / F_samp)
-F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
+t_sym = np.arange(0, Tsym, 1 / F_samp)
+F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t_sym))
 
-S_t = np.zeros(len(F_axis), dtype=np.complex)
-S_f = np.zeros(len(F_axis), dtype=np.complex)
-S_tk = np.zeros(len(F_axis), dtype=np.complex)
+S_t_w_CP = np.zeros((len(t_sym) + 16) * Num_Dta_chnk, dtype=np.complex)
+S_f_chnk = np.zeros(len(F_axis), dtype=np.complex)
+for chnk in range(Num_Dta_chnk):
+    # prepare the data in frequency domain:
+    skip = 0
+    for k in range(len(F_axis)):
+        if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
+            skip += 1
+            S_f_chnk[k] = 0
+        else:
+            S_f_chnk[k] = Tx_Dta[k - skip]
 
-# prepare the data in frequency domain:
-skip = 0
-for k in range(len(F_axis)):
-    if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-        skip += 1
-        S_f[k] = 0
-    else:
-        S_f[k] = Tx_Dta[k - skip]
+    # prepare time domain signal using IFFT:
+    S_t_chnk = np.fft.ifft(S_f_chnk, n=64)
 
-# prepare time domain signal using IFFT:
-S_t = np.fft.ifft(S_f, n=64)
+    # single OFDM symbol in Frequency domain
+    # plt.figure()
+    # plt.plot(F_axis, np.abs(S_f_chnk))
+    # plt.xlabel('Frequency')
+    # plt.ylabel('S(f)')
+    # plt.grid()
+    # plt.title('OFDM symbol in frequency domain')
+    # plt.show()
 
-# OFDM symbol in Frequency domain
-# plt.figure()
-# plt.plot(F_axis, np.abs(S_f))
-# plt.xlabel('Frequency')
-# plt.ylabel('S(f)')
-# plt.grid()
-# plt.title('OFDM symbol in frequency domain')
-# plt.show()
+    # single OFDM symbol in Time domain
+    # plt.figure()
+    # plt.plot(t, S_t_chnk)
+    # plt.xlabel('Time')
+    # plt.ylabel('S(t)')
+    # plt.grid()
+    # plt.title('OFDM symbol in time domain')
+    # plt.show()
 
-# OFDM symbol in Time domain
-# plt.figure()
-# plt.plot(t, S_t)
-# plt.xlabel('Time')
-# plt.ylabel('S(t)')
-# plt.grid()
-# plt.title('OFDM symbol in time domain')
-# plt.show()
+    # Parallel to serial: ?
 
-# Parallel to serial: ?
+    # inserting cyclic prefix:
+    S_t_chnk_w_CP = np.zeros(int(len(S_t_chnk) + 16), dtype=np.complex)
+    CP = S_t_chnk[range(int(len(S_t_chnk) - 16), len(S_t_chnk))]
+    S_t_chnk_w_CP[range(16)] = CP
+    S_t_chnk_w_CP[range(16, len(S_t_chnk_w_CP))] = S_t_chnk
 
-# inserting cyclic prefix:
-S_t_w_CP = np.zeros(int(len(S_t) + 16), dtype=np.complex)
-CP = S_t[range(int(len(S_t) - 16), len(S_t))]
-S_t_w_CP[range(16)] = CP
-S_t_w_CP[range(16, len(S_t_w_CP))] = S_t
+    t_sym_w_CP = np.arange(0, Tsym + GI, 1 / F_samp)  # new Symbol time includes cyclic prefix
 
-t_w_CP = np.arange(0, Tsym + GI, 1 / F_samp)  # new Symbol time includes cyclic prefix
+    # plt.figure()
+    # plt.plot(t_sym_w_CP, S_t_chnk_w_CP)
+    # plt.xlabel('Time')
+    # plt.ylabel('S(t) with GI')
+    # plt.title('single OFDM symbol with CP in time domain')
+    # plt.grid()
+    # plt.show()
 
+    ######################## D/A converter:#############################################################
+    # up sample by factor of 32
+    # (S_t_w_CP_up32, t_w_CP_up32) = signal.resample(S_t_w_CP, 32 * len(S_t_w_CP), t_w_CP, domain='time')
+    #
+    # plt.figure()
+    # plt.plot(t_w_CP_up32, S_t_w_CP_up32)
+    # plt.xlabel('Time')
+    # plt.ylabel('S(t) with GI')
+    # plt.title('upsampled OFDM symbol with CP in time domain')
+    # plt.grid()
+    # plt.show()
+    ###################################################################################################
+    S_t_w_CP[range(chnk*len(S_t_chnk_w_CP), (chnk+1)*len(S_t_chnk_w_CP))] = S_t_chnk_w_CP
+
+t_w_CP = np.arange(0, (Tsym + GI) * Num_Dta_chnk, 1 / F_samp)
 
 # plt.figure()
 # plt.plot(t_w_CP, S_t_w_CP)
 # plt.xlabel('Time')
 # plt.ylabel('S(t) with GI')
-# plt.title('OFDM symbol with CP in time domain')
+# plt.title('OFDM signal with CP in time domain')
 # plt.grid()
 # plt.show()
-
-###########################next step#################################
-# D/A converter:
-# up sample by factor of 5
-# S_t_w_CP_up = signal.resample_poly(S_t_w_CP, 5, 1)
-# (S_t_w_CP_up32, t_w_CP_up32) = signal.resample(S_t_w_CP, 32 * len(S_t_w_CP), t_w_CP, domain='time')
-
-# plt.figure()
-# plt.plot(t_w_CP_up32, S_t_w_CP_up32)
-# plt.xlabel('Time')
-# plt.ylabel('S(t) with GI')
-# plt.title('Transmitted upsampled OFDM symbol with CP in time domain')
-# plt.grid()
-# plt.show()
-##########################################################################################
 
 
 # Rx
 def OFDM_FFT_Rx():
+    Dta_vec = np.zeros(56 * Num_Dta_chnk, dtype=np.complex)
+    Dta_I = np.zeros(56 * Num_Dta_chnk, dtype=np.float)
+    Dta_Q = np.zeros(56 * Num_Dta_chnk, dtype=np.float)
+
     ################next step###########################################################
     # D/A
     # Rx_Sig_w_CP = S_t_w_CP_up32
@@ -136,6 +148,7 @@ def OFDM_FFT_Rx():
     gamma_b_dB_Max = 20
     SER_vec = np.zeros(gamma_b_dB_Max + 1, dtype=np.float)
     for gamma_b_dB in range(gamma_b_dB_Max + 1):
+    # for gamma_b_dB in range(gamma_b_dB_Max, gamma_b_dB_Max + 1):    # for debug for single SNR/bit value
         gamma_b_L = 10 ** (0.1 * gamma_b_dB)
         N0_Discrete = Eb_Discrete / gamma_b_L
         Ni_Discrete = np.sqrt(N0_Discrete / 2) * np.random.normal(loc=0, scale=1,
@@ -155,44 +168,44 @@ def OFDM_FFT_Rx():
         # plt.grid()
         # plt.show()
 
-        # Remove CP
-        Sig_t = R_t_Disc_w_CP[range(16, len(R_t_Disc_w_CP))]
-
-        # plt.figure()
-        # plt.plot(t, Sig_t)
-        # plt.xlabel('Time')
-        # plt.ylabel('S(t)')
-        # plt.title('Rx OFDM symbol in time domain')
-        # plt.grid()
-        # plt.show()
-
         # S/P Converter
+        for chnk in range(Num_Dta_chnk):
+            R_t_Disc_chnk_w_CP = R_t_Disc_w_CP[range(int(chnk * (len(R_t_Disc_w_CP)/Num_Dta_chnk)), int((chnk + 1) * (len(R_t_Disc_w_CP)/Num_Dta_chnk)))]
 
-        # FFT Block:
-        Sig_f = np.fft.fft(Sig_t, n=64)
+            # Remove CP
+            Sig_t_chnk = R_t_Disc_chnk_w_CP[range(16, len(R_t_Disc_chnk_w_CP))]
+            # plt.figure()
+            # plt.plot(t, Sig_t)
+            # plt.xlabel('Time')
+            # plt.ylabel('S(t)')
+            # plt.title('single Rx OFDM symbol in time domain')
+            # plt.grid()
+            # plt.show()
 
-        # OFDM symbol in Frequency domain
-        # plt.figure()
-        # plt.plot(F_axis, np.abs(Sig_f))
-        # plt.xlabel('Frequency')
-        # plt.ylabel('S(f)')
-        # plt.grid()
-        # plt.title('Rx OFDM symbol in frequency domain')
-        # plt.show()
+            # FFT Block:
+            Sig_f_chnk = np.fft.fft(Sig_t_chnk, n=64)
 
-        # P/S converter
-        Dta_vec = np.zeros(56, dtype=np.complex)
-        Dta_I = np.zeros(56, dtype=np.float)
-        Dta_Q = np.zeros(56, dtype=np.float)
+            # OFDM symbol in Frequency domain
+            # plt.figure()
+            # plt.plot(F_axis, np.abs(Sig_f_chnk))
+            # plt.xlabel('Frequency')
+            # plt.ylabel('S(f)')
+            # plt.grid()
+            # plt.title('Rx OFDM symbol in frequency domain')
+            # plt.show()
 
-        skip = 0
-        for k in range(len(F_axis)):
-            if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-                skip += 1
-            else:
-                Dta_vec[k - skip] = Sig_f[k]
+            # P/S converter
+            Dta_vec_chnk = np.zeros(56, dtype=np.complex)
+            skip = 0
+            for k in range(len(F_axis)):
+                if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
+                    skip += 1
+                else:
+                    Dta_vec_chnk[k - skip] = Sig_f_chnk[k]
 
-        # print(Dta_vec)
+                # print(Dta_vec_chnk)
+
+            Dta_vec[range(chnk*len(Dta_vec_chnk), (chnk+1)*len(Dta_vec_chnk))] = Dta_vec_chnk
 
         # Demapper - descision circle
 
--- OFDM_Tx.py
+++ OFDM_Tx.py
@@ -4,17 +4,19 @@ from commpy.filters import rrcosfilter
 from scipy import signal
 import math
 
-# random 56 symbols of data
+
+Num_Dta_chnk = 10  # number of data chunks
+# random 56 symbols of data per packet
 rng = np.random.default_rng()
 
 # 16QAM
 M = 16
 # infase data
-m_i = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
+m_i = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56*Num_Dta_chnk, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
 # quadrature data
-m_q = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
+m_q = 2 * rng.integers(1, high=int(np.sqrt(M)), size=56*Num_Dta_chnk, dtype=np.int64, endpoint=True) - 1 - int(np.sqrt(M))
 
-Dta = m_i + 1j * m_q
+Dta_Tx = m_i + 1j * m_q
 
 GI = 0.8 * 1e-6  # 0.8[uS] Long GI
 Tsym = 3.2 * 1e-6  # 3.2 [uS] symbol time
@@ -22,8 +24,8 @@ Delta_F = 1 / Tsym
 F = 1 / (Tsym + GI)  # analog Frequency
 F_samp = 20 * 1e6  # sample frquency 20MHz
 
-t = np.arange(0, Tsym, 1 / F_samp)
-F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t))
+t_sym = np.arange(0, Tsym, 1 / F_samp)
+F_axis = np.arange(-F_samp / 2, F_samp / 2, F_samp / len(t_sym))
 
 
 def OFDM_Oscilator_Tx():
@@ -148,72 +150,83 @@ def OFDM_Oscilator_Tx():
 
 
 def OFDM_FFT_Tx():
-    S_t = np.zeros(len(F_axis), dtype=np.complex)
-    S_f = np.zeros(len(F_axis), dtype=np.complex)
-    S_tk = np.zeros(len(F_axis), dtype=np.complex)
+    S_t_w_CP = np.zeros((len(t_sym) + 16) * Num_Dta_chnk, dtype=np.complex)
+    S_f_chnk = np.zeros(len(F_axis), dtype=np.complex)
+    for chnk in range(Num_Dta_chnk):
+        Dta_Tx_chnk = Dta_Tx[range(chnk * 56, (chnk+1) * 56)]
+        # prepare the data in frequency domain:
+        skip = 0
+        for k in range(len(F_axis)):
+            if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
+                skip += 1
+                S_f_chnk[k] = 0
+            else:
+                S_f_chnk[k] = Dta_Tx_chnk[k - skip]
 
-    # prepare the data in frequency domain:
-    skip = 0
-    for k in range(len(F_axis)):
-        if (k - len(F_axis) / 2) < -28 or (k - len(F_axis) / 2) == 0 or (k - len(F_axis) / 2) > 28:
-            skip += 1
-            S_f[k] = 0
-        else:
-            S_f[k] = Dta[k - skip]
+        # prepare time domain signal using IFFT:
+        S_t_chnk = np.fft.ifft(S_f_chnk, n=64)
 
-    # prepare time domain signal using IFFT:
-    S_t = np.fft.ifft(S_f, n=64)
+        # single OFDM symbol in Frequency domain
+        # plt.figure()
+        # plt.plot(F_axis, np.abs(S_f_chnk))
+        # plt.xlabel('Frequency')
+        # plt.ylabel('S(f)')
+        # plt.grid()
+        # plt.title('OFDM symbol in frequency domain')
+        # plt.show()
 
-    # OFDM symbol in Frequency domain
-    # plt.figure()
-    # plt.plot(F_axis, np.abs(S_f))
-    # plt.xlabel('Frequency')
-    # plt.ylabel('S(f)')
-    # plt.grid()
-    # plt.title('OFDM symbol in frequency domain')
-    # plt.show()
+        # single OFDM symbol in Time domain
+        # plt.figure()
+        # plt.plot(t, S_t_chnk)
+        # plt.xlabel('Time')
+        # plt.ylabel('S(t)')
+        # plt.grid()
+        # plt.title('OFDM symbol in time domain')
+        # plt.show()
 
-    # OFDM symbol in Time domain
-    # plt.figure()
-    # plt.plot(t, S_t)
-    # plt.xlabel('Time')
-    # plt.ylabel('S(t)')
-    # plt.grid()
-    # plt.title('OFDM symbol in time domain')
-    # plt.show()
+        # Parallel to serial: ?
 
-    # Parallel to serial: ?
+        # inserting cyclic prefix:
+        S_t_chnk_w_CP = np.zeros(int(len(S_t_chnk) + 16), dtype=np.complex)
+        CP = S_t_chnk[range(int(len(S_t_chnk) - 16), len(S_t_chnk))]
+        S_t_chnk_w_CP[range(16)] = CP
+        S_t_chnk_w_CP[range(16, len(S_t_chnk_w_CP))] = S_t_chnk
 
-    # inserting cyclic prefix:
-    S_t_w_CP = np.zeros(int(len(S_t) + 16), dtype=np.complex)
-    CP = S_t[range(int(len(S_t) - 16), len(S_t))]
-    S_t_w_CP[range(16)] = CP
-    S_t_w_CP[range(16, len(S_t_w_CP))] = S_t
+        t_sym_w_CP = np.arange(0, Tsym + GI, 1 / F_samp)  # new Symbol time includes cyclic prefix
 
-    t_w_CP = np.arange(0, Tsym + GI, 1 / F_samp)  # new Symbol time includes cyclic prefix
+        # plt.figure()
+        # plt.plot(t_sym_w_CP, S_t_chnk_w_CP)
+        # plt.xlabel('Time')
+        # plt.ylabel('S(t) with GI')
+        # plt.title('single OFDM symbol with CP in time domain')
+        # plt.grid()
+        # plt.show()
 
-    # plt.figure()
-    # plt.plot(t_w_CP, S_t_w_CP)
-    # plt.xlabel('Time')
-    # plt.ylabel('S(t) with GI')
-    # plt.title('OFDM symbol with CP in time domain')
-    # plt.grid()
-    # plt.show()
+        ######################## D/A converter:#############################################################
+        # up sample by factor of 32
+        # (S_t_w_CP_up32, t_w_CP_up32) = signal.resample(S_t_w_CP, 32 * len(S_t_w_CP), t_w_CP, domain='time')
+        #
+        # plt.figure()
+        # plt.plot(t_w_CP_up32, S_t_w_CP_up32)
+        # plt.xlabel('Time')
+        # plt.ylabel('S(t) with GI')
+        # plt.title('upsampled OFDM symbol with CP in time domain')
+        # plt.grid()
+        # plt.show()
+        ###################################################################################################
+        S_t_w_CP[range(chnk*len(S_t_chnk_w_CP), (chnk+1)*len(S_t_chnk_w_CP))] = S_t_chnk_w_CP
 
-    # D/A converter:
-    # up sample by factor of 5
-    # S_t_w_CP_up = signal.resample_poly(S_t_w_CP, 5, 1)
-    (S_t_w_CP_up32, t_w_CP_up32) = signal.resample(S_t_w_CP, 32 * len(S_t_w_CP), t_w_CP, domain='time')
-
+    t_w_CP = np.arange(0, (Tsym + GI) * Num_Dta_chnk, 1 / F_samp)
     plt.figure()
-    plt.plot(t_w_CP_up32, S_t_w_CP_up32)
+    plt.plot(t_w_CP, S_t_w_CP)
     plt.xlabel('Time')
     plt.ylabel('S(t) with GI')
-    plt.title('upsampled OFDM symbol with CP in time domain')
+    plt.title('OFDM signal with CP in time domain')
     plt.grid()
     plt.show()
 
-    return S_t_w_CP_up32
+    return S_t_w_CP
+
 
 
 def main():
